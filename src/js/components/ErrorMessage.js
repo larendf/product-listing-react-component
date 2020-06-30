ae626382729a9a79877180e98f3f5e42a681223d
laren-akqa <laren.delafuente@akqa.com>
2020-06-30 14:23:03 +1000

    clean up code

--- src/js/components/ErrorMessage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ErrorMessage = props => <div className="error-message">{props.errorMessage}</div>;
-
-export default ErrorMessage;
--- src/js/components/Promo.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Promo = props => <span className={"promo " + props.type}>{props.label}</span>;
-
-export default Promo;
